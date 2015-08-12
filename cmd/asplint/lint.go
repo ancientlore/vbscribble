b33a0e55907f82f149f34f631d82ca7612ca3a69
Michael Lore <michael.lore@gmail.com>
2015-08-12 18:16:03 -0400

    Ability to find objects

--- cmd/asplint/lint.go
+++ cmd/asplint/lint.go
@@ -8,11 +8,14 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
 	var root string
+	var obj bool
 	flag.StringVar(&root, "root", ".", "Root folder to search")
+	flag.BoolVar(&obj, "obj", false, "Show COM objects used in each file")
 	flag.Parse()
 
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -37,20 +40,36 @@ func main() {
 					}
 				}()
 				lex.Init(fil, f, vbscanner.HTML_MODE)
-				for k, t, _ := lex.Lex(); k != vblexer.EOF; k, t, _ = lex.Lex() {
+				creatingObj := false
+				for k, t, v := lex.Lex(); k != vblexer.EOF; k, t, v = lex.Lex() {
 					switch k {
 					case vblexer.STATEMENT:
 						switch t {
 						case "Stop":
-							messages = append(messages, fmt.Sprintf("%d: Stop should not be used in production code", lex.Line))
+							messages = append(messages, fmt.Sprintf("%d: Statement [Stop] should not be used in production code", lex.Line))
 						case "Execute", "Executeglobal":
-							messages = append(messages, fmt.Sprintf("%d: %s is not recommended", lex.Line, t))
+							messages = append(messages, fmt.Sprintf("%d: Statement [%s] is not recommended", lex.Line, t))
 						}
 					case vblexer.FUNCTION:
 						switch t {
 						case "Eval":
-							messages = append(messages, fmt.Sprintf("%d: %s is not recommended", lex.Line, t))
+							messages = append(messages, fmt.Sprintf("%d: Function [%s] is not recommended", lex.Line, t))
 						}
+					case vblexer.IDENTIFIER:
+						switch strings.ToLower(v) {
+						case "createobject", "server.createobject", "wscript.createobject":
+							creatingObj = true
+						default:
+							if creatingObj && obj {
+								messages = append(messages, fmt.Sprintf("%d: Using object [%s]", lex.Line, v))
+							}
+							creatingObj = false
+						}
+					case vblexer.STRING:
+						if creatingObj && obj {
+							messages = append(messages, fmt.Sprintf("%d: Using object [%s]", lex.Line, v))
+						}
+						creatingObj = false
 					}
 				}
 				if len(messages) > 0 {
